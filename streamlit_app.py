6f28067ed4ee53aad953156006c45194ba9d77a6
dassabyasachi14 <104012707+dassabyasachi14@users.noreply.github.com>
2024-06-17 19:17:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("Hello-World")
+st.write("# Streamlit Calculator")
+number1 = st.number_input("First Number")
+number2 = st.number_input("Second Number")
+num3=number1+number2
+st.write("Output is",num3)
